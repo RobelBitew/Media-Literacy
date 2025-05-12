b74ceac2cc05583e9ecdc6e702100f9ec1bf7e78
morrUni <morri243@wwu.edu>
2025-05-11 22:14:49 -0700

    added basic loggin

--- FastAPI/app/routes.py
+++ FastAPI/app/routes.py
@@ -2,6 +2,24 @@ from fastapi import APIRouter, HTTPException, Form
 #from app.scraper 
 import instaloader
 import os
+import logging
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s'))
+
+    fh = logging.FileHandler(os.path.join(LOG_DIR, "app.log"))
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
 
 SESSION_DIR = "sessions"
 os.makedirs(SESSION_DIR, exist_ok=True)
@@ -15,8 +33,10 @@ def login_instagram(username: str = Form(...), password: str = Form(...)):
         L.login(username, password)
         session_path = os.path.join(SESSION_DIR, f"session-{username}")
         L.save_session_to_file(session_path)
+        logger.info(f"Session saved for user: {username}")
         return {"message": f"Session for {username} saved successfully."}
     except Exception as e:
+        logger.error(f"Login failed for {username}")
         raise HTTPException(status_code=401, detail=f"Login failed")
 
 @router.get("/followees/{username}")
@@ -33,4 +53,5 @@ def get_followees(username: str):
         followees = [f.username for f in profile.get_followees()]
         return {"followees": followees}
     except Exception as e:
+        logger.error(f"Follower fetch failed: {username} threw bot flag")
         raise HTTPException(status_code=500, detail=str(e))
