3b6b6039dbc8bbdf6ef04452cf03b2e2a3e1937f
bitewr <bitewr@wwu.edu>
2025-05-30 13:32:17 -0700

    added poll

--- js/popup.js
+++ js/popup.js
@@ -101,3 +101,64 @@ function createInsightListItem(post, index) {
 
   return li;
 }
+
+//poll
+
+let poll = {
+  question: "Do you agree with the analysis?",
+  answers: ["yes", "no"],
+  pollCount: 20,
+  answersWeight: [10, 10],
+  selectedAnswer: -1,
+};
+
+let pollDOM = {
+  question: document.querySelector(".poll .question"),
+  answers: document.querySelector(".poll .answers"),
+};
+
+pollDOM.question.innerText = poll.question;
+pollDOM.answers.innerHTML = poll.answers
+  .map(function (answer, i) {
+    return `
+    <div class="answer" onclick="markAnswer('${i}')">
+    ${answer}
+    <span class="percentage-bar"></span>
+    <span class="percentage-value"></span>
+    </div>
+    `;
+  })
+  .join("");
+
+function markAnswer(i) {
+  poll.selectedAnswer = +i;
+  try {
+    document
+      .querySelector(".poll .answers .answer.selected")
+      .classList.remove("selected");
+  } catch (msg) {}
+  document
+    .querySelectorAll(".poll .answers .answer")
+    [+i].classList.add("selected");
+  showResults();
+}
+
+function showResults() {
+  let answers = document.querySelectorAll(".poll .answers .answer");
+  for (let i = 0; i < answers.length; i++) {
+    let percentage = 0;
+    if (i == poll.selectedAnswer) {
+      percentage = Math.round(
+        ((poll.answersWeight[i] + 1) * 100) / (poll.pollCount + 1)
+      );
+    } else {
+      percentage = Math.round(
+        (poll.answersWeight[i] * 100) / (poll.pollCount + 1)
+      );
+    }
+
+    answers[i].querySelector(".percentage-bar").style.width = percentage + "%";
+    answers[i].querySelector(".percentage-value").innerText = percentage + "%";
+  }
+}
+
